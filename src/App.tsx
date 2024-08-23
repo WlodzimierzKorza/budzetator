597efed3907c45b40fd0c6f9689a876d87329d2d
Włodzimierz <Korza>
2024-08-23 19:59:22 +0200

    Create app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Dashboard from "./dashboard/Dashboard";
+import { Route, Routes } from "react-router";
+import { routs } from "./routs";
+import { BrowserRouter } from "react-router-dom";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Dashboard>
+        <Routes>
+          {routs.map((route) => {
+            return <Route element={<route.component />} path={route.path} />;
+          })}
+        </Routes>
+      </Dashboard>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
