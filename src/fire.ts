8273283dcd8cef0922cc06c00cd25185e63995ce
Włodzimierz <Korza>
2024-08-24 08:23:28 +0200

    Add firebase to project

---
+++ src/fire.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBq6gRkyn2vZt975Na9T5wYgwHDIGLvJE",
+  authDomain: "budzetator.firebaseapp.com",
+  projectId: "budzetator",
+  storageBucket: "budzetator.appspot.com",
+  messagingSenderId: "1089356967088",
+  appId: "1:1089356967088:web:c05d341867b0f40462df75",
+};
+
+const app = initializeApp(firebaseConfig);
