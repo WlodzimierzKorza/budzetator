0be68172d95bcd120d7ee7fb9acecd9782c55daf
Włodzimierz <Korza>
2024-08-26 17:07:01 +0200

    fix types

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,8 @@
 import { User as FirebaseUser } from "firebase/auth";
-import { FirebaseApp } from "firebase/app";
 
 export interface SessionState {
   fetching: boolean;
   logged: boolean;
-  app: FirebaseApp;
   user: FirebaseUser | null;
   setUser: (user: FirebaseUser | null) => void;
   setFetching: (fetching: boolean) => void;
