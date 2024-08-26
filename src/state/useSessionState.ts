cb9f48758554c8907404c3c82ea07457c54d69c2
Włodzimierz <Korza>
2024-08-26 10:14:33 +0200

    Fix

--- src/state/useSessionState.ts
+++ src/state/useSessionState.ts
@@ -3,21 +3,9 @@ import { create } from "zustand";
 import { SessionState } from "../types";
 import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDBq6gRkyn2vZt975Na9T5wYgwHDIGLvJE",
-  authDomain: "budzetator.firebaseapp.com",
-  projectId: "budzetator",
-  storageBucket: "budzetator.appspot.com",
-  messagingSenderId: "1089356967088",
-  appId: "1:1089356967088:web:c05d341867b0f40462df75",
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 export const useSessionState = create<SessionState>((set, get) => ({
   fetching: false,
   logged: false,
-  app,
   user: null,
   setUser: (user) => set(() => ({ user: user, logged: user ? true : false })),
   setFetching: (fetching) => set(() => ({ fetching })),
