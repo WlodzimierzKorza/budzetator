59c55d662f3db88e1a303fd47d796037555edd9b
Włodzimierz <Korza>
2024-08-26 10:14:08 +0200

    Implement firebase config from env

--- src/pages/state/useAuth.ts
+++ src/pages/state/useAuth.ts
@@ -1,21 +1,25 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 import { SignInProps } from "../../types";
 import { useSessionState } from "../../state/useSessionState";
 import { useSnackbar } from "notistack";
 import { showError, showSuccess } from "../../utils/snackbar-utils";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBq6gRkyn2vZt975Na9T5wYgwHDIGLvJE",
-  authDomain: "budzetator.firebaseapp.com",
-  projectId: "budzetator",
-  storageBucket: "budzetator.appspot.com",
-  messagingSenderId: "1089356967088",
-  appId: "1:1089356967088:web:c05d341867b0f40462df75",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 export const useAuth = () => {
   const { setUser, setFetching, setLogged } = useSessionState();
   const { enqueueSnackbar } = useSnackbar();
@@ -27,11 +31,15 @@ export const useAuth = () => {
         email,
         password
       );
-      setUser(userCredential.user);
-      showSuccess(enqueueSnackbar, "Logged");
+      const user = userCredential.user;
+      setUser(user);
+      localStorage.setItem("user", JSON.stringify(user));
+      showSuccess(
+        enqueueSnackbar,
+        `${user.displayName || user.email} został zalogowany`
+      );
     } catch (error) {
       showError(enqueueSnackbar, `Error signing in: ${error}`);
-      console.error("Error signing in:", error);
       setUser(null);
       setLogged(false);
     } finally {
@@ -42,15 +50,41 @@ export const useAuth = () => {
   const signOutUser = async () => {
     setFetching(true);
     try {
-      await signOut(getAuth());
+      await signOut(getAuth(app));
       setUser(null);
       setLogged(false);
+      setFetching(false);
+      localStorage.removeItem("user");
     } catch (error) {
-      console.error("Error signing out:", error);
+      showError(enqueueSnackbar, `Error signing out: ${error}`);
     } finally {
       setFetching(false);
     }
   };
+  const registerUser = async ({ email, password }: SignInProps) => {
+    setFetching(true);
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        getAuth(app),
+        email,
+        password
+      );
+      const user = userCredential.user;
+      setUser(user);
+      setLogged(true);
+      localStorage.setItem("user", JSON.stringify(user));
+      showSuccess(
+        enqueueSnackbar,
+        `${user.displayName || user.email} został zarejestrowany`
+      );
+      setFetching(false);
+    } catch (error) {
+      setFetching(false);
+      setLogged(false);
+      setUser(null);
+      showError(enqueueSnackbar, `Error signing out: ${error}`);
+    }
+  };
 
-  return { signIn, signOut: signOutUser };
+  return { signIn, signOutUser, registerUser };
 };
